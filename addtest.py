d9860b920ee6f03db384222d3b95b784efe5660c
Nathaniel Nabong <nathaniel.nabong@kyndryl.com>
2025-07-17 15:30:52 +0100

    py test demo

---
+++ addtest.py
@@ -0,0 +1,9 @@
+import pytest
+from addition import addition
+
+def test_addition():
+    assert addition(2, 3) == 5
+    assert addition(-1, 1) == 0
+    assert addition(0, 0) == 0
+    assert addition(-5, -5) == -10
+    assert addition(100, 200) == 300
